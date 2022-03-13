c294537b53d1c7e2278245a3428109f0b4a54281
Axitvadi <axitvadi@gmail.com>
2022-03-13 11:55:52 -0800

    route changes

--- routes/index.js
+++ routes/index.js
@@ -7,24 +7,30 @@ const user = require('./user')
 const detail = require('./detail')
 const { ensureauthorized } = require("../middleware/auth")
 
-router.use('/admin',admin)
-router.use('/user',user)
-router.use('/detail',detail)
+router.use('/admin', admin)
+router.use('/user', user)
+router.use('/detail', detail)
 
 // ---------------------get-method-------------------------------
-router.get('/about-us', (req, res) => res.render('about-us'));
-router.get('/contact-us', (req, res) => res.render('contact-us'));
+router.get('/about-us', (req, res) => {
+    return res.render('about-us.ejs')
+});
+router.get('/contact-us', (req, res) => {
+    return res.render('contact-us.ejs')
+});
 router.get('/', (req, res) => {
     return res.render('index.ejs');
 });
-router.get('/login', (req, res) => accountcontroller.data.getlogin(req,res));
-router.get('/logout', (req, res) => accountcontroller.data.logout(req,res));
+router.get('/login', (req, res) => accountcontroller.data.getlogin(req, res));
+router.get('/logout', (req, res) => accountcontroller.data.logout(req, res));
 
-router.get('/our-services', (req, res) => res.render('our-services'))
+router.get('/our-services', (req, res) => {
+    return res.render('our-services.ejs')
+})
 
-router.get('/signup', (req, res) => accountcontroller.data.getsignup(req,res));
+router.get('/signup', (req, res) => accountcontroller.data.getsignup(req, res));
 router.get('/admin/dashboard', ensureauthorized(['admin']), (req, res) => res.render('admindashboard'))
-router.get('/user', ensureauthorized(['user']), (req, res) =>{res.render('admintabledemo')})
+router.get('/user', ensureauthorized(['user']), (req, res) => { res.render('admintabledemo') })
 router.get('/admin/record', ensureauthorized(['admin']), (req, res) => res.render('record'))
 router.get('/admin/pending_task', ensureauthorized(['admin']), (req, res) => res.render('panding_task'))
 router.get('/admin/completed_task', ensureauthorized(['admin']), (req, res) => res.render('completed_task'))
@@ -34,7 +40,8 @@ router.get('/logout', (req, res) => accountcontroller.data.logout(req, res))
 router.get('/verified', (req, res) => usercontroller.data.userverification(req, res))
 // ------------------------------------post-method----------------------------------
 router.post('/signup', (req, res) => {
-   return accountcontroller.data.signup(req, res)})
+    return accountcontroller.data.signup(req, res)
+})
 router.post('/login', (req, res) => accountcontroller.data.login(req, res))
 
 module.exports = router;
